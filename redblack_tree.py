f5fb5bc8a8390e774ef961a76bc900beab28dfa1
slowdh <75673102+slowdh@users.noreply.github.com>
2021-01-20 22:25:48 +0900

    Update redblack_tree.py

--- redblack_tree.py
+++ redblack_tree.py
@@ -5,7 +5,7 @@ class RedBlackNode:
 
 class RedBlackTree:
     """
-    Red-Black-Tree guarantees that tree's height is upper bounded by O(log(n))
+    Red-Black-Tree guarantees that tree's height is always theta(log(n))
     """
 
     def __init__(self):
@@ -13,7 +13,7 @@ class RedBlackTree:
 
     def _search_helper(self, val, root):
         if root is None:
-            return None
+            raise ValueError(f'Node with value ({val}) doesn\'t exist.')
         if root.val == val:
             return root
         elif root.val < val:
@@ -26,27 +26,30 @@ class RedBlackTree:
             if root.right is None:
                 root.right = node_insert
                 node_insert.parent = root
-                self._restore_rbt_property(node=node_insert)
+                self._restore_rbt_property_after_insertion(node=node_insert)
             else:
                 self._insert_helper(node_insert=node_insert, root=root.right)
         else:
             if root.left is None:
                 root.left = node_insert
                 node_insert.parent = root
-                self._restore_rbt_property(node=node_insert)
+                self._restore_rbt_property_after_insertion(node=node_insert)
             else:
                 self._insert_helper(node_insert=node_insert, root=root.left)
 
     def _match_parent_reference(self, node_prev, node_curr):
-        if node_curr.parent is None:
+        # transfer node_prev and parent reference to node_curr
+        node_parent = node_prev.parent
+        if node_parent is None:
             self.root = node_curr
-            node_curr.parent = None
-            return
-        node_parent = node_curr.parent
-        if node_parent.left == node_prev:
-            node_parent.left = node_curr
         else:
-            node_parent.right = node_curr
+            if node_parent.left == node_prev:
+                node_parent.left = node_curr
+            else:
+                node_parent.right = node_curr
+        if node_curr is not None:
+            node_curr.parent = node_parent
+        node_prev.parent = None
 
     def _rotate_left(self, node):
         # node.right becomes a new root of a subtree
@@ -56,10 +59,8 @@ class RedBlackTree:
         if node_child.left is not None:
             node_child.left.parent = node
         node_child.left = node
-        node_child.parent = node.parent
-        node.parent = node_child
         self._match_parent_reference(node_prev=node, node_curr=node_child)
-        return node_child
+        node.parent = node_child
 
     def _rotate_right(self, node):
         # node.left becomes a new root of a subtree
@@ -69,12 +70,10 @@ class RedBlackTree:
         if node_child.right is not None:
             node_child.right.parent = node
         node_child.right = node
-        node_child.parent = node.parent
-        node.parent = node_child
         self._match_parent_reference(node_prev=node, node_curr=node_child)
-        return node_child
+        node.parent = node_child
 
-    def _restore_rbt_property(self, node):
+    def _restore_rbt_property_after_insertion(self, node):
         if node.color == 'black':
             return
         if node.parent is None:
@@ -92,7 +91,7 @@ class RedBlackTree:
                     node_parent.color = 'black'
                 else:
                     self._rotate_left(node=node_parent)
-                    self._restore_rbt_property(node=node_parent)
+                    self._restore_rbt_property_after_insertion(node=node_parent)
             elif node_grandparent.right == node_parent:
                 if node_parent.right == node:
                     self._rotate_left(node=node_grandparent)
@@ -100,12 +99,77 @@ class RedBlackTree:
                     node_parent.color = 'black'
                 else:
                     self._rotate_right(node=node_parent)
-                    self._restore_rbt_property(node=node_parent)
+                    self._restore_rbt_property_after_insertion(node=node_parent)
         else:
             assert node_uncle.color == 'red'
             node_grandparent.color = 'red'
             node_parent.color = node_uncle.color = 'black'
-            self._restore_rbt_property(node=node_grandparent)
+            self._restore_rbt_property_after_insertion(node=node_grandparent)
+
+    def _delete_helper(self, node_delete):
+        if node_delete.left is not None and node_delete.right is not None:
+            node_max = self._get_maximum_node_under_sub_tree(root=node_delete.left)
+            node_delete.val, node_max.val = node_max.val, node_delete.val
+            self._delete_helper(node_delete=node_max)
+        else:
+            # at least one of children is None
+            node_parent = node_delete.parent
+            node_child = node_delete.left if node_delete.right is None else node_delete.right
+            self._match_parent_reference(node_prev=node_delete, node_curr=node_child)
+            if node_child is None:
+                return
+            if node_delete.color == 'black':
+                if node_child is None or node_child.color == 'black':
+                    self._restore_rbt_property_after_deletion(node=node_child, node_parent=node_parent)
+                else:
+                    assert node_child.color == 'red'
+                    node_child.color = 'black'
+
+    def delete(self, val):
+        node_delete = self.search(val=val)
+        self._delete_helper(node_delete=node_delete)
+
+    def _restore_rbt_property_after_deletion(self, node, node_parent):
+        if node_parent is None:
+            node.color = 'black'
+            return
+        node_sibling = node_parent.left if node_parent.right == node else node_parent.right
+        assert node_sibling is not None
+        if node_sibling.color == 'red':
+            if node_parent.right == node_sibling:
+                self._rotate_left(node=node_parent)
+            else:
+                self._rotate_right(node=node_parent)
+            node_parent.color = 'red'
+            node_sibling.color = 'black'
+            self._restore_rbt_property_after_deletion(node=node, node_parent=node_parent)
+        else:
+            # node_sibling.color == 'black'
+            if node_parent.color == 'red' and (node_sibling.left is None or node_sibling.left.color == 'black') and (node_sibling.right is None or node_sibling.right.color == 'black'):
+                node_parent.color = 'black'
+                node_sibling.color = 'red'
+            elif node_parent.color == 'black' and (node_sibling.left is None or node_sibling.left.color == 'black') and (node_sibling.right is None or node_sibling.right.color == 'black'):
+                node_sibling.color = 'red'
+                self._restore_rbt_property_after_deletion(node=node_parent, node_parent=node_parent.parent)
+            else:
+                if node_parent.right == node_sibling:
+                    if node_sibling.right.color == 'red':
+                        self._rotate_left(node=node_parent)
+                        node_parent.color, node_sibling.color = node_sibling.color, node_parent.color
+                        node_sibling.right.color = 'black'
+                    else:
+                        self._rotate_right(node=node_sibling)
+                        node_sibling.left.color, node_sibling.color = node_sibling.color, node_sibling.left.color
+                        self._restore_rbt_property_after_deletion(node=node, node_parent=node_parent)
+                else:
+                    if node_sibling.left.color == 'red':
+                        self._rotate_right(node=node_parent)
+                        node_parent.color, node_sibling.color = node_sibling.color, node_parent.color
+                        node_sibling.right.color = 'black'
+                    else:
+                        self._rotate_left(node=node_sibling)
+                        node_sibling.right.color, node_sibling.color = node_sibling.color, node_sibling.right.color
+                        self._restore_rbt_property_after_deletion(node=node, node_parent=node_parent)
 
     def _inorder_traversal_under_sub_tree(self, root):
         if root is None:
@@ -127,11 +191,21 @@ class RedBlackTree:
             ret.append(level)
         return ret
 
+    def _get_maximum_node_under_sub_tree(self, root):
+        if root.right is None:
+            return root
+        return self._get_maximum_node_under_sub_tree(root.right)
+
     def search(self, val):
         if self.root is None:
-            return None
+            raise ValueError(f'Node with value ({val}) doesn\'t exist.')
         else:
             return self._search_helper(val=val, root=self.root)
+
+    def get_maximum_node(self):
+        if self.root is None:
+            return None
+        return self._get_maximum_node_under_sub_tree(root=self.root)
 
     def insert(self, val):
         node_insert = RedBlackNode(val=val)
@@ -163,4 +237,15 @@ bst.insert(40)
 bst.insert(4)
 bst.insert(6)
 bst.insert(7)
+print(bst.level_order_traversal_with_color())
+bst.delete(18)
+bst.delete(8)
+bst.delete(5)
+bst.delete(15)
+bst.delete(7)
+bst.delete(6)
+bst.delete(4)
+bst.delete(25)
+bst.delete(40)
+bst.delete(17)
 print(bst.level_order_traversal_with_color())
